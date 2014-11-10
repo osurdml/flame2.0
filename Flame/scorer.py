783dfea3e2ff62534289a7cfb937c17bfeafb521
caleytown <jeff.caley@gmail.com>
2014-11-10 12:35:27 -0800

    Delete scorer.py

--- Flame/scorer.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'Caleytown'
-from abc import ABCMeta, abstractmethod
-
-class Scorer:
-    __metaclass__ = ABCMeta
-
-    @abstractmethod
-    def calcScore(self,fireConsumer, fireData): pass
