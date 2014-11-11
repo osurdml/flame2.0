d54f1d4c9ee9a497997f9fae50f488482d49966c
Ryan Skeele <rycoske@gmail.com>
2014-11-10 21:05:57 -0800

    Frontier display now works!

--- Flame/Sim.py
+++ Flame/Sim.py
@@ -4,6 +4,7 @@ from TestFrontierConsumer import TestFrontierConsumer
 from FarsiteProducer import FarsiteProducer
 from TestScorer import TestScorer
 from Visualizer import Visualizer
+import time
 
 frontierConsumer = TestFrontierConsumer()
 frontierConsumer2 = TestFrontierConsumer()
@@ -11,9 +12,10 @@ frontierProducer = FarsiteProducer()
 scorer = TestScorer()
 i = 0
 frontierController = FrontierController(scorer,frontierProducer,[frontierConsumer, frontierConsumer2])
-visualizer = Visualizer(frontierController.frontierData)
+visualizer = Visualizer()
 
 while(i < 10000): #frontierController.hasData()):
     frontierController.tick()
     visualizer.vis(frontierController.frontierData)
     i = i +1
+    time.sleep(.1)
--- Flame/Visualizer.py
+++ Flame/Visualizer.py
@@ -1,20 +1,22 @@
 import pygame
 import numpy as np
+import osgeo.gdal
 
 class Visualizer():
-    def __init__(self, frontier):
+    def __init__(self):
         #startups
-        frontier.shape = np.shape(frontier)
+        toa_file = "ash1_raster.toa"
+        frontier = np.array(osgeo.gdal.Open(toa_file).ReadAsArray())
         pygame.init()
         pygame.display.set_caption("Flame2.0: Fire Simulator")
-        image = pygame.display.set_mode(frontier.shape, pygame.HWSURFACE | pygame.DOUBLEBUF)
+        self.screen = pygame.display.set_mode(frontier.shape, pygame.HWSURFACE | pygame.DOUBLEBUF)
 
     def vis(self, frontier):
         # publish state of fire, agents,hotspots, and actions?
-        print self
-        values = (frontier)#what to display
-        values[values == 0] = 0xFFFFFFF #background
-        image = pygame.Surface(self.frontier.shape)
+        values = (frontier).astype(np.uint32)  #what to display
+        values[values == 0] = 0xFFFFFF #background
+        image = pygame.Surface(frontier.shape)
         pygame.surfarray.blit_array(image, values)
-
-        screen.blit(image, (0,0))
+        
+        self.screen.blit(image, (0,0))
+        pygame.display.flip()
